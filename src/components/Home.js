7f5c0f6515b4df62761eece1dc0a6e3275f29b4a
jacqueschen1 <jacqueschen1@gmail.com>
2019-09-28 22:51:41 -0700

    event list and display basic info

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
 
 export default class Home extends Component {
-  state = {
-    events: null,
-  }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      events: null,
+      selected: -1,
+    }
+    this.handleChange = this.handleChange.bind(this);
+}
     componentDidMount() {
       fetch(process.env.REACT_APP_AMAZON_API+"/events/get", {
       })
@@ -16,20 +22,70 @@ export default class Home extends Component {
         console.log(this.state.events)
       })
     }
+    createDropdownItems(){
+      let items = [];
+      if (this.state.events != null) {
+          items.push(<option key={"-1"} 
+                        value={-1}>{" "}
+                    </option>)
+          for (let i = 0; i < this.state.events.length; i++) {
+              items.push(<option key={i} 
+                                 value={i}>{this.state.events[i].ename}
+                          </option>);
+          }
+      }
+      return items;
+  }
+
+    handleChange(event) {
+      if (event.target.value != -1) {
+        this.setState({selected: event.target.value});
+      }
+    }
 
     render() {
-      if (this.state.events === null){
+      let events = this.state.events;
+      let index = this.state.selected;
+
+      if (events === null){
         return (
           <div>
             <p>Loading...</p>
           </div>
       )}
-      else {
+      else if (index === -1) {
         return (
           <div>
-            <h1>Events</h1>
-            {this.state.events.map(event => <p>{event.ename}</p>)}
-          </div>
+            <h1>Events Check-in</h1>     
+
+        <form> 
+        <label>
+            Select Event:  
+            <select onChange={this.handleChange}>
+            {this.createDropdownItems()}
+            </select>
+        </label>
+        </form>
+
+        </div>
+        );
+      } else {
+        return (
+          <div>
+            <h1>Events Check-in</h1>     
+
+        <form> 
+        <label>
+            Select Event:  
+            <select value={this.state.selected} onChange={this.handleChange}>
+            {this.createDropdownItems()}
+            </select>
+        </label>
+        </form>
+        <p># Registered: {events[index].regNum}</p>
+        <p># Checked In: {events[index].checkedNum}</p>
+        <p># Waitlisted: {events[index].waitNum}</p>
+        </div>
         );
       }
   }
