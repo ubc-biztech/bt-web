0712a0536d3647a3a00819e947afc76ce4565af3
Ian <ianmmah@gmail.com>
2019-09-29 01:19:54 -0700

    very slow but working datatable

--- src/components/Sheet.js
+++ src/components/Sheet.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react'
-import { DataTable } from 'carbon-components-react';
+import { DataTable, Table, TableRow, TableBody, TableCell, TableHead, TableHeader, TableContainer } from 'carbon-components-react';
 import './Home.scss';
 
 export default class Sheet extends Component {
   constructor(props) {
     super(props)
-    if (this.props.users) {
-      this.state = {
-        users: Object.keys(this.props.users)
+    console.log(this.props.users)
+    let users = Object.keys(this.props.users);
+    let rowData = [];
+    users.forEach(thing => {
+      rowData[thing] = {
+        id: thing,
+        status: 'something'
       }
+    })
+    this.state = {
+      users: rowData
     }
+    console.log(this.state)
   }
 
   render() {
@@ -19,13 +27,36 @@ export default class Sheet extends Component {
         <DataTable
         rows={this.state.users}
         headers={[
-          {key: 'name',header: 'Name'},
-          {key: 'protocol',header: 'Protocol'},
-          {key: 'port',header: 'Port'},
+          {key: 'id',header: 'Name'},
+          {key: 'status',header: 'Status'},
         ]}
         useZebraStyles={false}
         size={null}
         stickyHeader={false}
+        render={({ rows, headers, getHeaderProps }) => (
+                <TableContainer title="DataTable">
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        {headers.map(header => (
+                          <TableHeader {...getHeaderProps({ header })}>
+                            {header.header}
+                          </TableHeader>
+                        ))}
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {rows.map(row => (
+                        <TableRow key={row.id}>
+                          {row.cells.map(cell => (
+                            <TableCell key={cell.id}>{cell.value}</TableCell>
+                          ))}
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              )}
       />
     </div>
     )
