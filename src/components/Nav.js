d5ecdab1852249289d8ed87e0279f203e16f658e
Ian <ianmmah@gmail.com>
2019-10-14 19:54:51 -0700

    add side drawer

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,63 @@
-import React, { Component } from 'react'
+import React from 'react';
+import {
+  Drawer,
+  IconButton,
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+  Toolbar,
+  Typography,
+  AppBar
+} from '@material-ui/core';
+import { ChevronLeft, Menu } from '@material-ui/icons';
 
-export default class Router extends Component {
+export default function Router(props) {
 
-    render() {
-        let events = this.props.events;
-        const createEventLinks =
-                events.map(event =>
-                    <p>
-                      {event.ename}
-                    </p>
-                )
+  const events = props.events
 
-        return (
-            <div>
-              {createEventLinks}
-            </div>
-        )
-    }
+  const [open, setOpen] = React.useState(false);
 
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+          >
+            <Menu />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            BizTech App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="persistent" open={open} anchor="left">
+        <div>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeft />
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {events.map((event) => (
+            <ListItem button key={event.ename}>
+              <ListItemText primary={event.ename} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </div>
+  );
 }
