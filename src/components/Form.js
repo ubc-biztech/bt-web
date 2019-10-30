7949906189c93dcda25d92a583c1d6a1df170635
Ian <ianmmah@gmail.com>
2019-10-30 00:26:05 -0700

    Use formik

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,60 +1,86 @@
 import React from "react"
 import { Button, TextField } from "@material-ui/core";
-import { useFormFields } from "../libs/hooks";
 
-export default function Form() {
-    const [fields, handleFieldChange] = useFormFields({
-        email: "",
-        password: "",
-        confirmPassword: "",
-        name: "",
-        id: ""
-    });
+export default function Form(props) {
+    const {
+        values: { name, email, password, confirmPassword, id },
+        errors,
+        touched,
+        handleSubmit,
+        handleChange,
+        isValid,
+        setFieldTouched
+    } = props;
+
+    const change = (name, e) => {
+        e.persist();
+        handleChange(e);
+        setFieldTouched(name, true, false);
+    };
 
     return (
-        <form>
+        <form
+            onSubmit={handleSubmit}
+        >
             <TextField
                 label="Email"
                 autoComplete="email"
+                helperText={touched.email ? errors.email : ""}
+                error={touched.email && Boolean(errors.email)}
                 id="email"
-                value={fields.email}
-                onChange={handleFieldChange}
+                value={email}
+                onChange={change.bind(null, "email")}
                 onKeyPress={e => handleKey(e)} />
             <br />
             <TextField
                 label="Name"
                 autoComplete="name"
+                helperText={touched.name ? errors.name : ""}
+                error={touched.name && Boolean(errors.name)}
                 id="name"
-                value={fields.name}
-                onChange={handleFieldChange}
+                value={name}
+                onChange={change.bind(null, "name")}
                 onKeyPress={e => handleKey(e)} />
             <br />
             <TextField
                 label="Student ID"
+                helperText={touched.id ? errors.id : ""}
+                error={touched.id && Boolean(errors.id)}
                 id="id"
-                value={fields.id}
-                onChange={handleFieldChange}
+                value={id}
+                onChange={change.bind(null, "id")}
                 onKeyPress={e => handleKey(e)} />
             <br />
             <TextField
                 label="Password"
                 type="password"
                 autoComplete="new-password"
+                helperText={touched.password ? errors.password : ""}
+                error={touched.password && Boolean(errors.password)}
                 id="password"
-                value={fields.password}
-                onChange={handleFieldChange}
+                value={password}
+                onChange={change.bind(null, "password")}
                 onKeyPress={e => handleKey(e)} />
             <br />
             <TextField
                 label="Confirm Password"
                 type="password"
-                id="passwordConfirm"
-                value={fields.passwordConfirm}
-                onChange={handleFieldChange}
+                id="confirmPassword"
+                helperText={touched.confirmPassword ? errors.confirmPassword : ""}
+                error={touched.confirmPassword && Boolean(errors.confirmPassword)}
+                value={confirmPassword}
+                onChange={change.bind(null, "confirmPassword")}
                 onKeyPress={e => handleKey(e)} />
             <br />
             <br />
-            <Button variant="contained" >Sign up</Button>
+            <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                disabled={!isValid}
+            >
+                Sign up
+            </Button>
         </form>
     )
 
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,81 +1,35 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Auth } from "aws-amplify";
-import { object, string, number } from 'yup';
-import { useFormFields } from "../libs/hooks";
+import * as Yup from "yup"
+import { Formik } from "formik";
 import Form from './Form'
 
 export default function SignUp() {
 
-    const schema = object().shape({
-        passwordConfirm: string().min(6).required("Please confirm your password"),
-        password: string().min(6).required(),
-        id: number('Valid Student ID required')
+    const validationSchema = Yup.object({
+        confirmPassword: Yup.string().min(6).required("Please confirm your password"),
+        password: Yup.string().min(6).required(),
+        id: Yup.number('Valid Student ID required')
             .min(9999999, 'Valid Student ID required')
             .max(100000000, 'Valid Student ID required')
             .required(),
-        name: string().required(),
-        email: string().email().required()
+        name: Yup.string().required(),
+        email: Yup.string().email().required()
     });
 
-    const [fields, handleFieldChange] = useFormFields({
-        email: "",
-        password: "",
-        confirmPassword: "",
-        name: "",
-        id: ""
-    });
-
-    const [isLoading, setIsLoading] = useState(false);
-
-    function validate() {
-        setIsLoading(true);
-
-        schema
-            .validate({
-                email: fields.email,
-                password: fields.password,
-                passwordConfirm: fields.passwordConfirm,
-                name: fields.name,
-                id: fields.id
-            })
-            .then(() => {
-                if (fields.password === fields.passwordConfirm)
-                    signIn()
-            })
-            .catch(err => {
-                console.log(err)
-                alert(err.errors[0]);
-                setIsLoading(false);
-            });
-    }
+    const values = { email: "", name: "", confirmPassword: "", password: "", id: "" };
 
     return (
-        <Form />
+        <Formik
+            render={props => <Form {...props} />}
+            initialValues={values}
+            validationSchema={validationSchema}
+            onSubmit={submitValues}
+        />
     )
 
-    async function signIn() {
-
-        try {
-            await Auth.signUp({
-                username: fields.email,
-                password: fields.password,
-                attributes: {
-                    email: fields.email,
-                    name: fields.fname,
-                    nickname: fields.id
-                }
-            });
-            alert("Signed up");
-            setIsLoading(false);
-        } catch (e) {
-            alert(e.message);
-            setIsLoading(false);
-        }
-
+    function submitValues(e) {
+        console.log(e)
     }
 
-    function handleKey(e) {
-        if (e.key === 'Enter')
-            validate()
-    }
 }
