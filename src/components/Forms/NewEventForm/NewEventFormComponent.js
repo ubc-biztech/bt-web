177e74bcaa58ded3e7f055936e7f9b1ca160051c
Ian <ianmmah@gmail.com>
2019-12-08 20:59:55 -0800

    remove unused var

--- src/components/Forms/NewEventForm/NewEventFormComponent.js
+++ src/components/Forms/NewEventForm/NewEventFormComponent.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Paper from "@material-ui/core/Paper";
