ae8565272e8a69ad0019cf3479465f3efcb9742e
andyl2020 <andyl2020@yahoo.ca>
2020-02-01 17:54:19 -0800

    killed all login components except for sign in with google. Added a card

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -2,15 +2,14 @@ import React from 'react'
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardActions from '@material-ui/core/CardActions';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import { Divider } from '@material-ui/core';
 import Container from '@material-ui/core/Container';
 
 import { Auth } from "aws-amplify";
@@ -26,23 +25,6 @@ const styles = {
     }
 };
 
-// export default function Login() {
-
-//     return (
-//         <div>
-//             <h1> Login </h1>
-//             <Button onClick={() => Auth.federatedSignIn({ provider: 'Google' })} variant="contained" color="primary">
-//                 <div style={styles.left}>
-//                     <img style={styles.socialIcon} alt="Google" src="./google.png" />
-//                 </div>
-//                 Sign In with Google
-//             </Button>
-//         </div >
-//     )
-
-// }
-
-
 const useStyles = makeStyles(theme => ({
   paper: {
     marginTop: theme.spacing(8),
@@ -52,7 +34,7 @@ const useStyles = makeStyles(theme => ({
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: theme.palette.primary.main,
   },
   form: {
     width: '100%', // Fix IE 11 issue.
@@ -60,6 +42,25 @@ const useStyles = makeStyles(theme => ({
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
+  },
+  root: {
+    borderRadius: 12,
+    minWidth: 256,
+    textAlign: 'center',
+  },
+  header: {
+    textAlign: 'center',
+    spacing: 10,
+  },
+  list: {
+    padding: '20px',
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+  action: {
+    display: 'flex',
+    justifyContent: 'space-around',
   },
 }));
 
@@ -70,73 +71,38 @@ export default function Login() {
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-          <div>
-            <Button onClick={() => Auth.federatedSignIn({ provider: 'Google' })} variant="contained" color="primary">
-                    <div style={styles.left}>
-                        <img style={styles.socialIcon} alt="Google" src="./google.png" />
-                    </div>
-                    Sign In with Google
+        <Card className={classes.root} width="400px">
+            <div style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center"}}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+            </div>
+            <CardHeader title="Sign in" className={classes.header} />
+            <Divider variant="middle" />
+            <CardContent>
+                <Button onClick={() => Auth.federatedSignIn({ provider: 'Google' })} variant="contained" color="primary">
+                        <div style={styles.left}>
+                            <img style={styles.socialIcon} alt="Google" src="./google.png" />
+                        </div>
+                        Sign In with Google
                 </Button>   
-          </div>
-        </form>
+            </CardContent>
+            <Divider variant="middle" />
+            <CardActions className={classes.action}>
+                <Typography variant="h8" align="center">Only @ubcbiztech gmails are valid</Typography>
+            </CardActions>
+        </Card>
       </div>
     </Container>
   );
 }
+
+
+
+
 
 
 
