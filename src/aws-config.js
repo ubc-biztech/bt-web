c31b85ab1e5088d0359c57c1f15277c0867f1944
Ian <ianmmah@gmail.com>
2020-02-02 21:55:11 -0800

    Fix 404 error on prod

--- src/aws-config.js
+++ src/aws-config.js
@@ -1,18 +1,18 @@
 /**
  * CLIENT URL
  */
-export const CLIENT_URL = process.env.REACT_APP_STAGE === 'production'
+const CLIENT_URL = process.env.REACT_APP_STAGE  === 'production'
     ? "https://app.ubcbiztech.com/"
     : "http://localhost:3000/"
 
 const awsmobile = {
     "aws_project_region": "us-west-2",
-    "aws_cognito_identity_pool_id": process.env.REACT_APP_AWS_COGNITO_POOL_ID,
+    "aws_cognito_identity_pool_id": "us-west-2:19fd1364-9f5b-487b-b169-5db0cf6e5a23",
     "aws_cognito_region": "us-west-2",
-    "aws_user_pools_id": process.env.REACT_APP_AWS_USER_POOLS_ID,
-    "aws_user_pools_web_client_id": process.env.REACT_APP_AWS_USER_POOLS_WEB_CLIENT_ID,
+    "aws_user_pools_id": "us-west-2_wfv5hw4OC",
+    "aws_user_pools_web_client_id": "hsnlooj9jf4mnh7m40dcufpg4",
     "oauth": {
-        "domain": process.env.REACT_APP_OATH_DOMAIN,
+        "domain": "bt-web-dev.auth.us-west-2.amazoncognito.com",
         "scope": [
             "phone",
             "email",
