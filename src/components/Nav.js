17eb2d3555372f3afb40027ba91ec10359dc86c5
Ian <ianmmah@gmail.com>
2020-02-08 11:15:49 -0800

    Icon sidebar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,97 +1,65 @@
 import React from "react";
 import {
   Drawer,
-  IconButton,
   Divider,
   List,
   ListItem,
-  ListItemText,
-  Toolbar,
-  Typography,
-  AppBar
 } from "@material-ui/core";
 import "./Nav.scss";
 import { setEvent } from "../actions/PageActions";
 import { connect } from "react-redux";
-import ChevronLeft from "@material-ui/icons/ChevronLeft";
-import Menu from "@material-ui/icons/Menu";
+import { makeStyles } from '@material-ui/core/styles';
+import HomeIcon from '@material-ui/icons/Home';
 import { Logout } from "./Authentication";
 import { useHistory } from "react-router-dom";
 
+const useStyles = makeStyles(theme => ({
+  drawer: {
+  }
+}))
+
 function Nav(props) {
   const history = useHistory();
-
-  const events = props.events;
-
-  const [open, setOpen] = React.useState(false);
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
+  const classes = useStyles();
 
   const handleItemClick = event => {
     props.setEvent(event);
     if (event.id)
       history.push({ pathname: "/event", search: "?id=" + event.id });
     else history.push("/");
-    setOpen(false);
   };
 
   return (
     <div>
-      <AppBar position="fixed">
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-          >
-            <Menu />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            BizTech Admin Dashboard{" "}
-            {props.event ? "- " + props.event.ename : ""}
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer variant="persistent" open={open} anchor="left">
-        <div>
-          <IconButton onClick={() => setOpen(false)}>
-            <ChevronLeft />
-          </IconButton>
-        </div>
+      <Drawer variant="permanent" className={classes.drawer}>
         <Divider />
-        <ListItem
-          button
-          selected={!props.event}
-          component="a"
-          onClick={handleItemClick}
-        >
-          <ListItemText primary="Home" />
-        </ListItem>
-        <br></br>
+        <List>
+          <ListItem onClick={handleItemClick} ariaLabekl="home" button>
+            <HomeIcon />
+          </ListItem>
+        </List>
+        {/* 
         <Typography className="menu-tag" variant="h6" noWrap>
           Events
         </Typography>
         <List>
           {events
             ? events.map(event => (
-                <ListItem
-                  button
-                  selected={props.event ? event.id === props.event.id : false}
-                  key={event.ename}
-                  component="a"
-                  onClick={handleItemClick.bind(this, event)}
-                >
-                  <ListItemText primary={event.ename} />
-                </ListItem>
-              ))
+              <ListItem
+                button
+                selected={props.event ? event.id === props.event.id : false}
+                key={event.ename}
+                component="a"
+                onClick={handleItemClick.bind(this, event)}
+              >
+                <ListItemText primary={event.ename} />
+              </ListItem>
+            ))
             : "Loading..."}
           <Logout />
-        </List>
+        </List> */}
       </Drawer>
-    </div>
+    </div >
   );
 }
 
