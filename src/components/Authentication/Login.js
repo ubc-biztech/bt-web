052c3ef69495d90c9f346fb09c3916abf10c142d
Superzzp <zhangzhipian1@gmail.com>
2020-04-10 21:26:10 -0400

    remove unused package CardMedia

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -6,7 +6,7 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardActions from '@material-ui/core/CardActions';
-import CardMedia from '@material-ui/core/CardMedia';
+//import CardMedia from '@material-ui/core/CardMedia';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
