5fe7c8e63592b7ad251115f79ceffa8523c70b2e
Ian Mah <ian.mah@hootsuite.com>
2020-05-13 11:11:18 -0700

    No need to export API_URL and API_KEY

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,10 +2,6 @@ import aws_config from '../aws-config';
 import Store from '../components/Store'
 import { setEvents } from '../actions/PageActions'
 
-export const API_URL = process.env.REACT_APP_STAGE  === 'production'
-    ? process.env.REACT_APP_PROD_API
-    : process.env.REACT_APP_STAGING_API
-
 // TODO: Configure travis to build a staging version
 // export const AWS_CONFIG = process.env.REACT_APP_STAGE === 'production'
 //     ? aws_config
@@ -13,7 +9,11 @@ export const API_URL = process.env.REACT_APP_STAGE  === 'production'
 
 export const AWS_CONFIG = aws_config
 
-export const API_KEY = process.env.REACT_APP_STAGE  === 'production'
+const API_URL = process.env.REACT_APP_STAGE  === 'production'
+    ? process.env.REACT_APP_PROD_API
+    : process.env.REACT_APP_STAGING_API
+    
+const API_KEY = process.env.REACT_APP_STAGE  === 'production'
     ? process.env.REACT_APP_PROD_API_KEY
     : process.env.REACT_APP_STAGING_API_KEY
 
