1a9b41bc795411251a81cfff674bb7ab89c1747a
Derek Chen <dchen150@users.noreply.github.com>
2020-09-03 21:38:02 -0700

    adjusted user home container to adjust based on screen size

--- src/pages/member/UserHome.js
+++ src/pages/member/UserHome.js
@@ -12,10 +12,10 @@ import UserProgress from '../../components/UserProgress'
 
 const useStyles = makeStyles({
   container: {
-    maxWidth: '1200px',
+    maxWidth: '85%',
     display: 'flex',
     flexWrap: 'wrap',
-    margin: 'auto',
+    margin: '75px auto',
     padding: '14px'
   },
   header: {
