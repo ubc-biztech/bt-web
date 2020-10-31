672e164049e765269444d2af58c9e24b05c7b83c
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2020-10-31 06:51:58 -0700

    remove ui actions

--- src/constants/_constants/actions.js
+++ src/constants/_constants/actions.js
@@ -5,9 +5,3 @@ export const SET_EVENTS_REGISTERED = 'SET_EVENTS_REGISTERED'
 // user actions
 export const SET_USER = 'SET_USER'
 export const LOGOUT = 'LOGOUT'
-
-// UI actions
-export const START_ACTION = 'START_ACTION'
-export const STOP_ACTION = 'STOP_ACTION'
-export const START_REFRESH_ACTION = 'START_REFRESH_ACTION'
-export const STOP_REFRESH_ACTION = 'STOP_REFRESH_ACTION'
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux'
 
 import eventReducer from './event/eventReducer'
-import uiReducer from './ui/uiReducer'
 import userReducer from './user/userReducer'
 
 export default combineReducers({
   eventState: eventReducer,
-  uiState: uiReducer,
   userState: userReducer
 })
