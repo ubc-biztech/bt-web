a007644177b84908efb82735a85a8f3e4238dbb7
Derek Chen <45786074+dchen150@users.noreply.github.com>
2020-12-27 19:10:51 -0800

    show /login page, change maxVP toggle parsing, prevent user login (only allow exec login)

--- src/utils/_utils/checkFeatureFlag.js
+++ src/utils/_utils/checkFeatureFlag.js
@@ -1,5 +1,5 @@
 export const checkFeatureFlag = (flag) => {
   if (!flag) return true
   const featureSpecified = process.env[flag]
-  return !!featureSpecified
+  return featureSpecified === 'true'
 }
