9560f4962ff40788617b58455e75a49e7c4b9666
Patrick Zhang <patrickjyzhang@gmail.com>
2021-06-14 22:47:55 -0700

    WIP stickers page

--- src/pages/member/Stickers/index.js
+++ src/pages/member/Stickers/index.js
@@ -1,55 +1,155 @@
-import { Typography, Paper, Grid, Avatar, Box } from '@material-ui/core';
+import {
+  Button,
+  Typography,
+  Paper,
+  Grid,
+  Box,
+  TextField,
+  Tooltip,
+  Divider,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import React from 'react';
+import React from "react";
 
-import houseImage from '../../../assets/house.svg';
+import houseImage from "../../../assets/house.svg";
+
+import { COLORS } from "../../../constants/_constants/theme";
 
 const useStyles = makeStyles({
-    container: {
-      maxWidth: "90%",
-      display: "flex",
-      flexWrap: "wrap",
-      margin: "75px 110px 75px 0px",
-      padding: "25px",
-    },
-    title: {
-      fontSize: '36px',
-    },
-    stickerImage: {
-      border: '3px solid gold',
-      height: '125px',
-      width: '125px',
-    },
-  });
+  container: {
+    maxWidth: "90%",
+    display: "flex",
+    flexDirection: "column",
+    flexWrap: "wrap",
+    margin: "75px 110px 75px 0px",
+    padding: "50px 75px",
+  },
+  title: {
+    fontSize: "36px",
+  },
+  stickerImage: {
+    border: `3px solid ${COLORS.LIGHT_YELLOW}`,
+    height: "125px",
+    width: "125px",
+  },
+  sticker: {
+    height: "150px",
+    width: "150px",
+    borderRadius: "100%",
+    border: "3px solid gold",
+  },
+  stickerInfo: {
+    padding: "8px",
+  },
+  unlockedText: {
+    color: `${COLORS.LIGHT_YELLOW}`,
+  },
+  divider: {
+    background: "white",
+  },
+});
+
+function StickerCodeForm(props) {
+  const onSubmit = () => console.log("Submitting sticker code!");
+
+  const classes = useStyles();
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              // defaultValue={initialValues.fname}
+              variant="outlined"
+              label="Enter Sticker Code"
+              // helperText={textFieldError("fname")}
+              // error={!!textFieldError("fname")}
+              id="stickerCode"
+              onChange={() => console.log("changing sticker code")}
+              fullWidth
+            />
+          </Grid>
+          <Button
+            // className={classes.button}
+            variant="contained"
+            color="primary"
+            type="submit"
+          >
+            Submit
+          </Button>
+        </Grid>
+      </form>
+    </>
+  );
+}
+
+function StickerInfo({ title, description }) {
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.stickerInfo}>
+      <Box mb={1}>
+        <Typography variant="h5">{title}</Typography>
+        <Divider className={classes.divider} />
+      </Box>
+      <Typography variant="body2">{description}</Typography>
+      <Typography variant="body2" className={classes.unlockedText}>
+        Unlocked!
+      </Typography>
+    </Box>
+  );
+}
+
+function Sticker({ src }) {
+  const classes = useStyles();
+
+  return (
+    <Tooltip
+      arrow
+      title={
+        <StickerInfo
+          title="Sticker Title"
+          description="Insert a silly description for this sticker here! For example, explain how to get this sticker."
+        />
+      }
+    >
+      <img src={src} className={classes.sticker}></img>
+    </Tooltip>
+  );
+}
 
 function Stickers(props) {
+  const classes = useStyles();
 
-    const classes = useStyles();
-    
-    let images = [];
-    for(let i = 0; i < 24; i++) {
-        images.push(houseImage);
-    }
-    
-    return (
-        <>
-        <Paper className={classes.container}>
-            <Box mb={4}>
-                <Typography className={classes.title} variant="h1">Sticker Collection</Typography>
-            </Box>
-            <Grid container spacing={5}>
-                {images.map((image) => {
-                    return (
-                        <Grid item md={2}>
-                            <Avatar src={image} className={classes.stickerImage}></Avatar>
-                        </Grid>
-                        );
-                    })}
-                
-            </Grid>
-        </Paper>
-        </>
-    )
+  const images = [];
+  for (let i = 0; i < 24; i++) {
+    images.push(houseImage);
+  }
+
+  return (
+    <>
+      <Paper className={classes.container}>
+        <Box mb={4}>
+          <Typography className={classes.title} variant="h1">
+            Sticker Collection
+          </Typography>
+        </Box>
+        <Box mb={4}>
+          <StickerCodeForm />
+        </Box>
+        <Grid container spacing={5}>
+          {images.map((image) => {
+            return (
+              <Grid key={image} item md={2}>
+                <Sticker src={image} />
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Paper>
+    </>
+  );
 }
 
 export default Stickers;
