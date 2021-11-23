57dcc265a5205981d4058f0395c92c8c90d16011
Patrick Zhang <patrickjyzhang@gmail.com>
2021-11-23 13:08:12 -0800

    fix registrationForm select field UI

--- src/pages/public/Event/EventRegister/EventRegisterForm.js
+++ src/pages/public/Event/EventRegister/EventRegisterForm.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react"
 import {
   Button,
   Grid,
@@ -10,34 +10,34 @@ import {
   FormControl,
   FormGroup,
   useTheme
-} from '@material-ui/core'
-import EventAvailableIcon from '@material-ui/icons/EventAvailable'
-import { makeStyles } from '@material-ui/core/styles'
+} from "@material-ui/core"
+import EventAvailableIcon from "@material-ui/icons/EventAvailable"
+import { makeStyles } from "@material-ui/core/styles"
 
-import { COLORS } from '../../../../constants/_constants/theme'
-import CustomTextField from '../../../../components/inputs/CustomTextField'
-import CustomSelect from '../../../../components/inputs/CustomSelect'
+import { COLORS } from "../../../../constants/_constants/theme"
+import CustomTextField from "../../../../components/inputs/CustomTextField"
+import CustomSelect from "../../../../components/inputs/CustomSelect"
 
 const useStyles = makeStyles((theme) => ({
   gridContainer: {
-    marginTop: '5px'
+    marginTop: "5px"
   },
   registerButton: {
-    textTransform: 'none'
+    textTransform: "none"
   },
   registerIcon: {
     color: COLORS.FONT_COLOR,
-    marginRight: '5px'
+    marginRight: "5px"
   },
   lumaContainer: {
-    width: '100%'
+    width: "100%"
   }
 }))
 
 export default function RegisterEventForm (props) {
   const classes = useStyles()
   const theme = useTheme()
-  const renderMobileOnly = useMediaQuery(theme.breakpoints.down('sm'))
+  const renderMobileOnly = useMediaQuery(theme.breakpoints.down("sm"))
 
   const {
     handleSubmit,
@@ -48,7 +48,7 @@ export default function RegisterEventForm (props) {
 
   return (
     <form onSubmit={handleSubmit}>
-      <Typography variant='caption' color={'error'}>
+      <Typography variant="caption" color={"error"}>
         * Indicates required field
       </Typography>
       <Grid className={classes.gridContainer} container spacing={3}>
@@ -56,12 +56,12 @@ export default function RegisterEventForm (props) {
           <FormControl>
             <FormGroup>
               <FormControlLabel
-                label='UBC Student'
+                label="UBC Student"
                 control={
                   <Checkbox
                     checked={isUBCStudent}
                     onChange={() => setIsUBCStudent(!isUBCStudent)}
-                    color='primary'
+                    color="primary"
                   />
                 } />
             </FormGroup>
@@ -71,119 +71,117 @@ export default function RegisterEventForm (props) {
         <Grid item xs={12}>
           <CustomTextField
             {...props}
-            label='First Name *'
-            groupName='fname'
-            autoComplete='given-name'
+            label="First Name *"
+            groupName="fname"
+            autoComplete="given-name"
           />
         </Grid>
 
         <Grid item xs={12}>
           <CustomTextField
             {...props}
-            label='Last Name *'
-            groupName='lname'
-            autoComplete='family-name'
+            label="Last Name *"
+            groupName="lname"
+            autoComplete="family-name"
           />
         </Grid>
 
         <Grid item xs={12}>
           <CustomTextField
             {...props}
-            label='Email Address *'
-            groupName='email'
-            autoComplete='email'
+            label="Email Address *"
+            groupName="email"
+            autoComplete="email"
           />
         </Grid>
 
         {isUBCStudent && <Grid item xs={12}>
           <CustomTextField
             {...props}
-            label='Student Number *'
-            groupName='id'
-            autoComplete='id'
+            label="Student Number *"
+            groupName="id"
+            autoComplete="id"
           />
         </Grid>}
 
         <Grid item xs={renderMobileOnly ? 12 : 8}>
-          <Grid item xs={12}>
-            <CustomSelect
-              {...props}
-              label='Faculty *'
-              listOfOptions={[
-                'Arts',
-                'Commerce',
-                'Science',
-                'Engineering',
-                'Kinesiology',
-                'Land and Food Systems',
-                'Forestry',
-                'Other',
-                'Not Applicable'
-              ]}
-              groupName='faculty'
-            />
-          </Grid>
+          <CustomSelect
+            {...props}
+            label="Faculty *"
+            listOfOptions={[
+              "Arts",
+              "Commerce",
+              "Science",
+              "Engineering",
+              "Kinesiology",
+              "Land and Food Systems",
+              "Forestry",
+              "Other",
+              "Not Applicable"
+            ]}
+            groupName="faculty"
+          />
+        </Grid>
 
-          <Grid item xs={12}>
-            <CustomSelect
-              {...props}
-              label='Level of study *'
-              listOfOptions={[
-                '1st Year',
-                '2nd Year',
-                '3rd Year',
-                '4th Year',
-                '5+ Year',
-                'Other',
-                'Not Applicable'
-              ]}
-              groupName='year'
-            />
-          </Grid>
+        <Grid item xs={renderMobileOnly ? 12 : 8}>
+          <CustomSelect
+            {...props}
+            label="Level of study *"
+            listOfOptions={[
+              "1st Year",
+              "2nd Year",
+              "3rd Year",
+              "4th Year",
+              "5+ Year",
+              "Other",
+              "Not Applicable"
+            ]}
+            groupName="year"
+          />
+        </Grid>
 
-          <Grid item xs={12}>
-            <CustomSelect
-              {...props}
-              label='Preferred Pronouns'
-              listOfOptions={[
-                'He/Him/His',
-                'She/Her/Hers',
-                'They/Them/Their',
-                'Prefer not to say'
-              ]}
-              groupName='gender'
-            />
-          </Grid>
+        <Grid item xs={renderMobileOnly ? 12 : 8}>
+          <CustomSelect
+            {...props}
+            label="Preferred Pronouns"
+            listOfOptions={[
+              "He/Him/His",
+              "She/Her/Hers",
+              "They/Them/Their",
+              "Prefer not to say"
+            ]}
+            groupName="gender"
+          />
+        </Grid>
 
-          <Grid item xs={12}>
-            <CustomSelect
-              {...props}
-              label='How did you hear about this event?'
-              listOfOptions={[
-                'Facebook',
-                'Instagram',
-                'LinkedIn',
-                'Boothing',
-                'Friends',
-                'BizTech Newsletter',
-                'Faculty Newsletter',
-                'Other'
-              ]}
-              groupName='heardFrom'
-            />
-          </Grid>
+        <Grid item xs={renderMobileOnly ? 12 : 8}>
+          <CustomSelect
+            {...props}
+            label="How did you hear about this event?"
+            listOfOptions={[
+              "Facebook",
+              "Instagram",
+              "LinkedIn",
+              "Boothing",
+              "Friends",
+              "BizTech Newsletter",
+              "Faculty Newsletter",
+              "Other"
+            ]}
+            groupName="heardFrom"
+          />
         </Grid>
       </Grid>
       <br />
       <Button
         className={classes.registerButton}
-        variant='contained'
-        color='primary'
-        type='submit'
+        variant="contained"
+        color="primary"
+        type="submit"
         disabled={isSubmitting}
       >
         <EventAvailableIcon
-          style={{ color: COLORS.BACKGROUND_COLOR, marginRight: '5px' }}
+          style={{ color: COLORS.BACKGROUND_COLOR, marginRight: "5px" }}
         />
         register
       </Button>
