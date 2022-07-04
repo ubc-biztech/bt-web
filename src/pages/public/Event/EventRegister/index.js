531967f4242f0d808ff8307f9f4a2f761e0d59c9
kaoshannon <kaoshannon10@gmail.com>
2022-07-04 16:29:01 -0400

    Fixed lint warnings, reverted yup package update

--- src/pages/public/Event/EventRegister/index.js
+++ src/pages/public/Event/EventRegister/index.js
@@ -1,33 +1,32 @@
-import React, { useState, Fragment } from 'react'
-import { Helmet } from 'react-helmet'
-import { Formik } from 'formik'
-import * as Yup from 'yup'
-import { withRouter, useHistory} from "react-router-dom";
+import React, { useState, Fragment } from "react";
+import { Helmet } from "react-helmet";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { useHistory } from "react-router-dom";
 
-import EventView from 'components/Event/EventView'
-import EventRegisterForm from './EventRegisterForm'
-import EventRegisterSuccess from './EventRegisterSuccess'
-import NotFound from 'pages/NotFound'
+import EventView from "components/Event/EventView";
+import EventRegisterForm from "./EventRegisterForm";
+import EventRegisterSuccess from "./EventRegisterSuccess";
+import NotFound from "pages/NotFound";
 
-import { makeStyles } from '@material-ui/core/styles'
-import { Grid, Paper, Typography } from '@material-ui/core'
-import { Skeleton } from '@material-ui/lab'
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Paper, Typography } from "@material-ui/core";
+import { Skeleton } from "@material-ui/lab";
 import { ArrowBack as ArrowBackIcon } from "@material-ui/icons";
 
-import { COLORS } from '../../../../constants/_constants/theme'
+import { COLORS } from "../../../../constants/_constants/theme";
 
-import { fetchBackend } from 'utils'
-
+import { fetchBackend } from "utils";
 
 const useStyles = makeStyles((theme) => ({
   layout: {
-    [theme.breakpoints.up('sm')]: {
+    [theme.breakpoints.up("sm")]: {
       width: 850,
-      margin: 'auto'
+      margin: "auto"
     }
   },
   paper: {
-    [theme.breakpoints.up('sm')]: {
+    [theme.breakpoints.up("sm")]: {
       margin: theme.spacing(3)
     }
   },
@@ -36,35 +35,35 @@ const useStyles = makeStyles((theme) => ({
   },
   registrationHeader: {
     borderLeft: `2px solid ${COLORS.BIZTECH_GREEN}`,
-    marginTop: '35px',
-    paddingLeft: '19px',
-    marginLeft: '11px'
+    marginTop: "35px",
+    paddingLeft: "19px",
+    marginLeft: "11px"
   },
   registrationText: {
-    fontWeight: 'bold',
-    fontSize: '24px'
-  }, 
+    fontWeight: "bold",
+    fontSize: "24px"
+  },
   ArrowBackIcon: {
-    color: COLORS.WHITE, 
+    color: COLORS.WHITE,
     fontSize: "40px"
   }
-}))
+}));
 
 const EventFormContainer = (props) => {
-  const classes = useStyles(); 
+  const classes = useStyles();
   const history = useHistory();
-  const { user, eventId, event, upcomingEvents, loading } = props
+  const { user, eventId, event, upcomingEvents, loading } = props;
 
   const initialRegistrationState = {
-    // set to true to see success page 
-    isRegistered: true,
+    // set to true to see success page
+    isRegistered: false,
     registeredEmail: undefined
-  }
+  };
 
   const [registration, setRegistration] = useState(initialRegistrationState);
   // const [isUBCStudent, setIsUBCStudent] = useState(true);
 
-  const resetRegistration = () => setRegistration(initialRegistrationState)
+  const resetRegistration = () => setRegistration(initialRegistrationState);
 
   const validationSchema = Yup.object({
     email: Yup.string().email().required(),
@@ -77,7 +76,7 @@ const EventFormContainer = (props) => {
     hopinStatus: Yup.string().required("This field is required"),
     // gender: Yup.string().required("This field is required"),
     // diet: Yup.string().required("This field is required"),
-    heardFrom: Yup.string().required("This field is required"),
+    heardFrom: Yup.string().required("This field is required")
   });
 
   // const UBCValidationSchema = Yup.object({
@@ -95,38 +94,38 @@ const EventFormContainer = (props) => {
   // });
 
   const initialValues = {
-    email: user.email || '',
-    fname: user.fname || '',
-    lname: user.lname || '',
-    year: user.year || '',
-    faculty: user.faculty || '',
-    major: user.major || '',
-    biztechMemberStatus: '',
-    hopinStatus: '',
+    email: user.email || "",
+    fname: user.fname || "",
+    lname: user.lname || "",
+    year: user.year || "",
+    faculty: user.faculty || "",
+    major: user.major || "",
+    biztechMemberStatus: "",
+    hopinStatus: "",
     // diet: "",
-    heardFrom: ''
+    heardFrom: ""
     // gender: "",
     // id: "",
-  }
+  };
 
-  const { isRegistered, registeredEmail } = registration
+  const { isRegistered, registeredEmail } = registration;
 
   if (loading) {
     return (
       <div className={classes.layout}>
         <Paper className={classes.paper}>
           <Skeleton
-            animation='wave'
-            variant='rect'
-            width={'100%'}
+            animation="wave"
+            variant="rect"
+            width={"100%"}
             height={320}
           />
           <div className={classes.content}>
             <Grid container spacing={3}>
               <Grid item xs={12}>
                 <Skeleton
-                  animation='wave'
-                  variant='rect'
+                  animation="wave"
+                  variant="rect"
                   width={300}
                   height={30}
                 />
@@ -136,17 +135,17 @@ const EventFormContainer = (props) => {
                 <Grid item container spacing={1} key={e}>
                   <Grid item xs={12}>
                     <Skeleton
-                      animation='wave'
-                      variant='rect'
+                      animation="wave"
+                      variant="rect"
                       width={130}
                       height={20}
                     />
                   </Grid>
                   <Grid item xs={12}>
                     <Skeleton
-                      animation='wave'
-                      variant='rect'
-                      width={'100%'}
+                      animation="wave"
+                      variant="rect"
+                      width={"100%"}
                       height={20}
                     />
                   </Grid>
@@ -155,8 +154,8 @@ const EventFormContainer = (props) => {
 
               <Grid item xs={12}>
                 <Skeleton
-                  animation='wave'
-                  variant='rect'
+                  animation="wave"
+                  variant="rect"
                   width={90}
                   height={36}
                 />
@@ -165,22 +164,21 @@ const EventFormContainer = (props) => {
           </div>
         </Paper>
       </div>
-    )
+    );
   }
   return event ? (
     <div className={classes.layout}>
-
       <ArrowBackIcon
-        cursor='pointer'
+        cursor="pointer"
         className={classes.ArrowBackIcon}
         onClick={() => history.push("/events/")}
-      /> 
+      />
 
       <Helmet>
         <title>{event.ename} - Register</title>
       </Helmet>
       <EventView event={event}>
-        // set isRegistered to true to see success screen 
+        {/* set isRegistered to true to see success screen */}
         {isRegistered ? (
           <EventRegisterSuccess
             email={registeredEmail}
@@ -207,7 +205,7 @@ const EventFormContainer = (props) => {
             >
               {(props) => {
                 props = {
-                  ...props,
+                  ...props
                   // isUBCStudent,
                   // setIsUBCStudent,
                 };
@@ -220,9 +218,9 @@ const EventFormContainer = (props) => {
     </div>
   ) : (
     <NotFound message={`The event with id ${eventId} could not be found!`} />
-  )
+  );
 
-  async function submitValues (values) {
+  async function submitValues(values) {
     const {
       email,
       fname,
@@ -233,7 +231,7 @@ const EventFormContainer = (props) => {
       biztechMemberStatus,
       hopinStatus,
       // diet,
-      heardFrom,
+      heardFrom
       // gender,
 
       // id,
@@ -248,7 +246,7 @@ const EventFormContainer = (props) => {
       email,
       year,
       faculty,
-      major,
+      major
       // gender,
       // diet,
     };
@@ -261,16 +259,16 @@ const EventFormContainer = (props) => {
       registrationStatus: "registered",
       // Careers in Tech specific fields
       biztechMemberStatus,
-      hopinStatus,
+      hopinStatus
     };
 
-    fetchBackend('/users', 'POST', body, false)
+    fetchBackend("/users", "POST", body, false)
       .catch((err) => {
         // If the error is not "User could not be created because it already exists"
         if (err.status !== 409) {
           alert(
-            'An error occured while trying to register. Please try again or contact UBC BizTech.'
-          )
+            "An error occured while trying to register. Please try again or contact UBC BizTech."
+          );
         }
       })
       .finally(() => {
@@ -280,26 +278,26 @@ const EventFormContainer = (props) => {
     // registerUser(registrationBody);
   }
 
-  async function registerUser (registrationBody) {
-    fetchBackend('/registrations', 'POST', registrationBody, false)
+  async function registerUser(registrationBody) {
+    fetchBackend("/registrations", "POST", registrationBody, false)
       .then(() => {
         // alert('Congratulations! You are now signed up.')
         setRegistration({
           ...registration,
           isRegistered: true,
           registeredEmail: registrationBody.email
-        })
+        });
       })
       .catch((err) => {
         if (err.status === 409) {
-          alert('You are already registered for this event.')
+          alert("You are already registered for this event.");
         } else {
           alert(
-            'An error occured while trying to register. Please try again or contact UBC BizTech.'
-          )
+            "An error occured while trying to register. Please try again or contact UBC BizTech."
+          );
         }
-      })
+      });
   }
-}
+};
 
-export default EventFormContainer
+export default EventFormContainer;
