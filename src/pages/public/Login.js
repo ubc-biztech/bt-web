a3a3623e76b21c27a10c870efe4ee65822b1e387
Voctor <voctorbidoof@gmail.com>
2022-07-20 17:09:38 -0700

    add basic forgot password to login page

--- src/pages/public/Login.js
+++ src/pages/public/Login.js
@@ -118,10 +118,13 @@ const styles = {
 function Login () {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [verificationCode, setVerificationCode] = useState('')
   const [errors, setErrors] = useState({
     emailError: '',
-    passwordError: ''
+    passwordError: '',
+    verificationCodeError: ''
   })
+  const [reset, setReset] = useState(false);
   const history = useHistory()
 
   const validateEmail = (value) => {
@@ -143,6 +146,70 @@ function Login () {
     return error
   }
 
+  const validateVerificationCode = (value) => {
+    let error = ''
+    if (!value && value.length != 6) {
+      error = 'Valid verification code is required'
+    }
+    return error
+  }
+
+  const resetPassword = () => {
+    Auth.forgotPassword(email)
+      .then(() => {
+        // setErrors({
+        //   ...errors,
+        //   emailError: 'Password reset email sent'
+        // })
+        setReset(true);
+        setErrors({
+          ...errors,
+          emailError: '',
+          passwordError: ''
+        })
+      }
+      )
+      .catch((err) => {
+        setErrors({
+          ...errors,
+          emailError: err.message
+        })
+      }
+      )
+  }
+
+  const resetPasswordSubmit = () => {
+    const emailError = validateEmail(email)
+    const passwordError = validatePassword(password)
+    const verificationCodeError = validateVerificationCode(verificationCode);
+
+    if (emailError || passwordError) {
+      // if any errors with inputs, set state and rerender (don't call signin/signup)
+      setErrors({
+        emailError: emailError,
+        passwordError: passwordError,
+        verificationError: verificationCodeError
+      })
+    } else {
+      Auth.forgotPasswordSubmit(email, verificationCode, password)
+      .then(() => {
+          setReset(false);
+          setErrors({
+            ...errors,
+            emailError: 'Password reset successful! Please login.'
+          })
+        }
+        )
+        .catch((err) => {
+          setErrors({
+            ...errors,
+            emailError: err.message
+          })
+        }
+      )
+    }
+  }
+
   const handleSubmit = async () => {
     const emailError = validateEmail(email)
     const passwordError = validatePassword(password)
@@ -150,6 +217,7 @@ function Login () {
     if (emailError || passwordError) {
       // if any errors with inputs, set state and rerender (don't call signin/signup)
       setErrors({
+        ...errors,
         emailError: emailError,
         passwordError: passwordError
       })
@@ -196,6 +264,7 @@ function Login () {
       {/* TODO: Maintenance message here for MinVP */}
       <div style={styles.columns}>
         <Card style={styles.card}>
+          { !reset ? 
           <CardContent>
             <Typography variant='h1' color='primary'>
               Sign In
@@ -257,12 +326,67 @@ function Login () {
               </Button>
             </form>
             <Typography style={styles.notAMember}>
+              Forgot your password?
+              <Link onClick={() => {resetPassword()}} style={styles.signUpLink}>
+                Reset password
+              </Link>
+            </Typography>
+            <Typography style={styles.notAMember}>
               Not a BizTech member yet?
               <Link to='/signup' style={styles.signUpLink}>
                 Sign up here!
               </Link>
             </Typography>
           </CardContent>
+          : <CardContent>
+            <Typography variant='h1' color='primary'> 
+              Password Reset Email Sent!
+            </Typography>
+            {/* enter the verification code */}
+            <Typography>
+              Please check your email for the verification code.
+            </Typography>
+            <form>
+              <Typography style={styles.emailLogin}>
+                Email:
+              </Typography>
+              <input
+                style={styles.inputText}
+                type='email'
+                email='email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)} />
+              <div style={styles.errors}>{errors.emailError}</div>
+              <Typography style={styles.emailLogin}>
+                Verification Code:
+              </Typography>
+              <input
+                style={styles.inputText}
+                type='email'
+                email='verification code'
+                value={verificationCode}
+                onChange={(e) => setVerificationCode(e.target.value)} />
+              <div style={styles.errors}>{errors.verificationCodeError}</div>
+            </form>
+            <form>
+              <Typography style={styles.emailLogin}>
+                New Password:
+              </Typography>
+              <input
+                style={styles.inputText}
+                type='password'
+                email='new password'
+                value={password} onChange={(e) => setPassword(e.target.value)} />
+              <div style={styles.errors}>{errors.passwordError}</div>
+              <Button
+                onClick={() => resetPasswordSubmit()}
+                style={styles.submitButton}
+              >
+                Reset Password
+              </Button>
+            </form>
+          </CardContent>
+        }
         </Card>
         <img src={LoginImage} alt='Computer' style={styles.loginImage} />
       </div>
