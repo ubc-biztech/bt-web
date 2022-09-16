f6a1aa46d607e4990f451443d3c70c5aead81ae6
alvinkam <alvin.kam.33@gmail.com>
2022-09-15 23:17:14 -0700

    adjustments to reg table for hello hacks

--- src/pages/admin/Event/EventStats/EventStatsTable.js
+++ src/pages/admin/Event/EventStats/EventStatsTable.js
@@ -408,13 +408,6 @@ export class EventStatsTable extends Component {
         cellStyle: { whiteSpace: "nowrap" }
       },
       {
-        title: "Student Number",
-        field: "studentId",
-        type: "numeric",
-        sorting: false,
-        cellStyle: { whiteSpace: "nowrap" }
-      },
-      {
         title: "Email",
         field: "id",
         sorting: false,
@@ -425,7 +418,38 @@ export class EventStatsTable extends Component {
         field: "diet",
         sorting: false,
         cellStyle: { whiteSpace: "nowrap" }
-      }
+      },
+      {
+        title: "Student Number",
+        field: "studentId",
+        type: "numeric",
+        sorting: false,
+        cellStyle: { whiteSpace: "nowrap" }
+      },
+      {
+        title: "Faculty",
+        field: "faculty",
+        sorting: false,
+        cellStyle: { whiteSpace: "nowrap" }
+      },
+      {
+        title: "Year Level",
+        field: "year",
+        sorting: false,
+        cellStyle: { whiteSpace: "nowrap" }
+      },
+      {
+        title: "Gender",
+        field: "gender",
+        sorting: false,
+        cellStyle: { whiteSpace: "nowrap" }
+      },
+      {
+        title: "Heard about event from",
+        field: "heardFrom",
+        sorting: false,
+        cellStyle: { whiteSpace: "nowrap" }
+      },
     ];
 
     const registrationColumns = defaultColumns.concat(this.state.columns);
@@ -589,6 +613,9 @@ const PopoverCell = (props) => {
           transformOrigin={{
             vertical: "top",
             horizontal: "left"
+          }}
+          PaperProps={{
+            style: { maxWidth: "50%" }
           }}
           onClose={handlePopoverClose}
           disableRestoreFocus
