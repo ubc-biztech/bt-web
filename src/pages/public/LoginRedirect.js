51524c1a390a7e10af68bd18ec59d24acd8091c7
kaoshannon <kaoshannon10@gmail.com>
2022-10-10 13:42:55 -0700

    reintroducing oauth-login

--- src/pages/public/LoginRedirect.js
+++ src/pages/public/LoginRedirect.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { useHistory, useLocation } from 'react-router-dom'
-import { Auth } from 'aws-amplify'
+import React from "react";
+import { connect } from "react-redux";
+import { useHistory, useLocation } from "react-router-dom";
+import { Auth } from "aws-amplify";
 
-import Loading from 'pages/Loading'
+import Loading from "pages/Loading";
 
-import { setUser, logout } from 'store/user/userActions'
-import { log, fetchBackend, checkFeatureFlag } from 'utils'
+import { setUser, logout } from "store/user/userActions";
+import { log, fetchBackend, checkFeatureFlag } from "utils";
 
 function LoginRedirect(props) {
-  const history = useHistory()
-  const location = useLocation()
+  const history = useHistory();
+  const location = useLocation();
 
   // we don't want to keep polling forever
   const timeoutRedirect = setTimeout(() => {
-    log('Timed out!')
-    history.push('/')
-  }, 6000)
+    log("Timed out!");
+    history.push("/");
+  }, 6000);
 
   const populateUserAndRedirect = (authUser, redirectRoute, admin = false) => {
     // Parse first name and last name
-    const initialName = authUser.name.split(' ')
-    const fname = initialName[0]
-    const lname = initialName[1]
+    const initialName = authUser.name.split(" ");
+    const fname = initialName[0];
+    const lname = initialName[1];
 
     // save only essential info to redux
     const userObject = admin
@@ -35,40 +35,51 @@ function LoginRedirect(props) {
           fname,
           lname,
           admin
-        }
-    props.setUser(userObject)
-    history.push(redirectRoute)
-  }
+        };
+    props.setUser(userObject);
+    history.push(redirectRoute);
+    console.log("AUTH USER HERE", authUser);
+  };
 
   const pollForAuthenticatedUser = () => {
     // currentSession includes more auth information (such as token) than currentAuthenticatedUser
     Auth.currentSession({ bypassCache: true })
       .then(async (session) => {
-        const authUser = session.idToken.payload
-        const { email } = authUser
+        const authUser = session.idToken.payload;
+        const { email } = authUser;
 
         // might have already set them to be an admin
-        const isAdminGroup = authUser['cognito:groups']?.includes('admin')
+        const isAdminGroup = authUser["cognito:groups"]?.includes("admin");
+        const isOauthGroup =
+          authUser["cognito:groups"]?.includes(
+            "us-west-2_w0R176hhp_Facebook"
+          ) ||
+          authUser["cognito:groups"]?.includes("us-west-2_w0R176hhp_Google");
+
         if (isAdminGroup) {
-          populateUserAndRedirect(authUser, location.state?.redirect || '/', true)
+          populateUserAndRedirect(
+            authUser,
+            location.state?.redirect || "/",
+            true
+          );
         } else if (
-          email.substring(email.indexOf('@') + 1, email.length) ===
-          'ubcbiztech.com'
+          email.substring(email.indexOf("@") + 1, email.length) ===
+          "ubcbiztech.com"
         ) {
           // if biztech email, set them as admin (no need for 'sign in')
           // attempt to assign cognito group 'admin' to this user
           // await fetchBackend('/admin', 'POST')
           //   .then(() => {})
           //   .catch((err) => console.log(err))
-          clearTimeout(timeoutRedirect)
+          clearTimeout(timeoutRedirect);
         }
         // If not biztech username (normal member)
         // if maxvp is hidden, log out because we only want to allow biztech exec logins
-        if (!checkFeatureFlag('REACT_APP_SHOW_MAXVP')) {
-          alert('Sorry, login currently restricted to biztech executives!')
-          await Auth.signOut()
-          await props.logout()
-          return null
+        if (!checkFeatureFlag("REACT_APP_SHOW_MAXVP")) {
+          alert("Sorry, login currently restricted to biztech executives!");
+          await Auth.signOut();
+          await props.logout();
+          return null;
         }
 
         // Detect if "first time sign up" by checking if custom:student_id is saved in the user pool
@@ -77,8 +88,8 @@ function LoginRedirect(props) {
         if (email) {
           try {
             // check database
-            const user = await fetchBackend(`/users/${email}`, 'GET')
-            clearTimeout(timeoutRedirect)
+            const user = await fetchBackend(`/users/${email}`, "GET");
+            clearTimeout(timeoutRedirect);
             const payload = {
               email: user.id,
               fname: user.fname,
@@ -92,47 +103,57 @@ function LoginRedirect(props) {
               heardFrom: user.heardFrom,
               gender: user.gender,
               admin: user.admin,
-              'favedEventsID;year': user['favedEventsID;year'],
+              "favedEventsID;year": user["favedEventsID;year"],
               isMember: user.isMember
-            }
-            props.setUser(payload) // save to redux
-            history.push(location.state?.redirect || '/') // Redirect to the 'user home' page
+            };
+            props.setUser(payload); // save to redux
+            history.push(location.state?.redirect || "/"); // Redirect to the 'user home' page
           } catch (err) {
-            console.log(err)
+            console.log(err);
             // if the user exists in the user pool, but not the database, remove the user pool's student_id
+            // if the user exists in the user pool but not the databse, then redirect to membership sign up form
             if (err.status === 404) {
-              clearTimeout(timeoutRedirect)
-              
+              clearTimeout(timeoutRedirect);
+
               // updateUserAttributes requires a user object and Auth.currentSession() does not provide the exact object needed
-              const user = await Auth.currentAuthenticatedUser()
-              await Auth.updateUserAttributes(user, {
-                'custom:student_id': ''
-              })
-              authUser['custom:student_id'] = null
-              populateUserAndRedirect(authUser, '/signup')
+              const user = await Auth.currentAuthenticatedUser();
+
+              if (isOauthGroup) {
+                await Auth.updateUserAttributes(user, {
+                  "custom:student_id": ""
+                });
+                authUser["custom:student_id"] = null;
+              }
+              populateUserAndRedirect(
+                authUser,
+                "/RegistrationForms/MembershipForms/MembershipForm.js"
+              );
             } else {
               console.log(
-                'Encountered an error querying database!',
+                "Encountered an error querying database!",
                 err.status
-              )
+              );
             }
           }
         } else {
-          clearTimeout(timeoutRedirect)
+          clearTimeout(timeoutRedirect);
           // If the user doesn't exist in the user pool, redirect to the 'user register' form
-          populateUserAndRedirect(authUser, '/signup')
+          populateUserAndRedirect(
+            authUser,
+            "/RegistrationForms/MembershipForms/MembershipForm.js"
+          );
         }
       })
       .catch(() => {
-        log('Not signed in')
-        setTimeout(() => pollForAuthenticatedUser(), 300)
-      })
-  }
+        log("Not signed in");
+        setTimeout(() => pollForAuthenticatedUser(), 300);
+      });
+  };
 
   // There is a time delay where Auth.currentSession() returns the wrong value. Hence polling
-  pollForAuthenticatedUser()
+  pollForAuthenticatedUser();
 
-  return <Loading message="Signing in..." />
+  return <Loading message="Signing in..." />;
 }
 
-export default connect(null, { setUser, logout })(LoginRedirect)
+export default connect(null, { setUser, logout })(LoginRedirect);
