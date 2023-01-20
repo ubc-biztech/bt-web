7f8b5fe26a9ab2cf4b19572759adb2a20d3abf84
Jerry Xu <jerryxu@Jerrys-MacBook-Pro.local>
2023-01-19 22:23:49 -0800

    added points field to registration table, refactored eventstatstable to extract columns and registrationfield. Broke partner form custom questions displaying

---
+++ src/pages/admin/Event/EventStats/PointsField.js
@@ -0,0 +1,87 @@
+import { TextField, Typography } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import { fetchBackend } from "../../../../utils";
+
+const PointsField = (props) => {
+    const [value, setValue] = useState(props.points);
+    const [isEditing, setIsEditing] = useState(false);
+
+    useEffect(() => {
+        setValue(props.points);
+    }, [props.points]);
+
+
+    const handleBlur = async (event) => {
+        if (event.type === "blur" || event.key === "Enter") {
+            setIsEditing(false);
+            
+            if (value !== props.points) {
+                changeUserPoints();
+            }
+        }
+    };
+
+    const changeUserPoints = async () => {
+        try {
+            validatePoints(value);
+
+            if (window.confirm("Are you sure you want to change the points value?")) {
+                await updateUserPoints(props.id, props.fname, value);
+            } else {
+                setValue(props.points);
+            }
+        } catch (error) {
+            console.log(error);
+            alert(error);
+            setValue(props.points);
+        }
+    }
+
+    const updateUserPoints = async (id, fname, points) => {
+        const body = {
+            eventID: props.eventID,
+            year: props.eventYear,
+            points: parseInt(points)
+        };
+        await fetchBackend(`/registrations/${id}/${fname}`, "PUT", body);
+
+        props.refreshTable();
+    }
+
+    const validatePoints = (points) => {
+        if (isNaN(points) || points == null || points === "") {
+            throw new Error("Points must be a number");
+        }
+        return;
+    }
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    };
+
+    const handleFocus = () => {
+        setIsEditing(true);
+    };
+
+    if (!isEditing) {
+        return (
+            <Typography
+                onClick={handleFocus}
+            >
+                {value}
+            </Typography>
+        )
+    } else {
+        return (
+            <TextField
+                autoFocus
+                value={value}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                onKeyPress={handleBlur}
+            />
+        )
+    }
+}
+
+export default PointsField;
