2121b4cc13cc19d194685402b8cf5c4083e80ee0
Voctor <voctorbidoof@gmail.com>
2023-04-09 18:15:58 -0700

    Remove uses of DraggableTitle from MaterialTable

--- src/pages/admin/Event/EventStats/DraggableTitle.js
+++ src/pages/admin/Event/EventStats/DraggableTitle.js
@@ -2,12 +2,11 @@ import React from 'react';
 import DragIndicatorIcon from '@material-ui/icons/DragIndicator';
 
 const DraggableTitle = ({ title }) => {
-    return (
-        <div style={{display: "flex", flexDirection: "row", justifyContent: 'center', alignItems: 'center'}}>
-            <DragIndicatorIcon />
-            <span>{title}</span>
-        </div>
-    );
+    return title;
+        // <div style={{display: "flex", flexDirection: "row", justifyContent: 'center', alignItems: 'center'}}>
+        //     <DragIndicatorIcon />
+        //     <span>{title}</span>
+        // </div>
 };
 
 export default DraggableTitle;
