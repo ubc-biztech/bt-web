085de38a4065d2282d64e37a4088299cd116bc67
KavyaDalmia <dalmia.kavya@gmail.com>
2023-09-28 14:02:56 -0700

    cleaned up comments and changes

--- src/pages/admin/Event/EventStats/EventStatsTable.js
+++ src/pages/admin/Event/EventStats/EventStatsTable.js
@@ -269,7 +269,6 @@ export class EventStatsTable extends Component {
     return heardFrom;
   }
 
-  // registration status stat
   registrationNumbers(users) {
     const registrationNumbers = {
     };
@@ -312,18 +311,12 @@ export class EventStatsTable extends Component {
           ? faculties[user.basicInformation?.faculty] + 1
           : 1;
       }
-      // the above code checks if the faculty that user entred is in the database, if 
-      // yes the it increments faculties by 1
-      // else, it sets it to 1.
 
       if (user.basicInformation?.year) {
         const yearInt = parseInt(user.basicInformation?.year);
         if (yearInt) {
           years[yearInt] = years[yearInt] ? years[yearInt] + 1 : 1;
         }
-        // to calcutate how many people for each year, 
-        // we check if the year entred exist, if yes then increment it by 1
-        // else set it to 1
       }
       if (user.basicInformation?.diet) {
         dietary[user.basicInformation?.diet] = dietary[
