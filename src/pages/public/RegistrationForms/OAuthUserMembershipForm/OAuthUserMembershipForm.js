e22b038b9de8e86a4c941e0123718f8d9e963b38
dennis <dennis34@students.cs.ubc.ca>
2023-11-23 16:37:38 -0800

    fixed styling issues

--- src/pages/public/RegistrationForms/OAuthUserMembershipForm/OAuthUserMembershipForm.js
+++ src/pages/public/RegistrationForms/OAuthUserMembershipForm/OAuthUserMembershipForm.js
@@ -191,23 +191,23 @@ export default function MembershipForm(props) {
 
         {(memberType === MEMBER_TYPES.UBC ||
           memberType === MEMBER_TYPES.UNIVERSITY) && (
-            <Grid item xs={12}>
-              <CustomSelect
-                {...props}
-                label="Level of study *"
-                listOfOptions={[
-                  "1st Year",
-                  "2nd Year",
-                  "3rd Year",
-                  "4th Year",
-                  "5+ Year",
-                  "Other",
-                  "Not Applicable"
-                ]}
-                groupName="year"
-              />
-            </Grid>
-          )}
+          <Grid item xs={12}>
+            <CustomSelect
+              {...props}
+              label="Level of study *"
+              listOfOptions={[
+                "1st Year",
+                "2nd Year",
+                "3rd Year",
+                "4th Year",
+                "5+ Year",
+                "Other",
+                "Not Applicable"
+              ]}
+              groupName="year"
+            />
+          </Grid>
+        )}
 
         {memberType === MEMBER_TYPES.HIGH_SCHOOL && (
           <Grid item xs={12}>
@@ -229,37 +229,37 @@ export default function MembershipForm(props) {
 
         {(memberType === MEMBER_TYPES.UBC ||
           memberType === MEMBER_TYPES.UNIVERSITY) && (
-            <Grid item xs={12}>
-              <CustomSelect
-                {...props}
-                label="Faculty *"
-                listOfOptions={[
-                  "Arts",
-                  "Commerce",
-                  "Science",
-                  "Engineering",
-                  "Kinesiology",
-                  "Land and Food Systems",
-                  "Forestry",
-                  "Other",
-                  "Not Applicable"
-                ]}
-                groupName="faculty"
-              />
-            </Grid>
-          )}
+          <Grid item xs={12}>
+            <CustomSelect
+              {...props}
+              label="Faculty *"
+              listOfOptions={[
+                "Arts",
+                "Commerce",
+                "Science",
+                "Engineering",
+                "Kinesiology",
+                "Land and Food Systems",
+                "Forestry",
+                "Other",
+                "Not Applicable"
+              ]}
+              groupName="faculty"
+            />
+          </Grid>
+        )}
 
         {(memberType === MEMBER_TYPES.UBC ||
           memberType === MEMBER_TYPES.UNIVERSITY) && (
-            <Grid item xs={12} className={classes.major}>
-              <CustomTextField
-                {...props}
-                label="Major *"
-                groupName="major"
-                autoComplete="major"
-              />
-            </Grid>
-          )}
+          <Grid item xs={12} className={classes.major}>
+            <CustomTextField
+              {...props}
+              label="Major *"
+              groupName="major"
+              autoComplete="major"
+            />
+          </Grid>
+        )}
 
         {memberType === MEMBER_TYPES.UBC && (
           <Grid item xs={12}>
