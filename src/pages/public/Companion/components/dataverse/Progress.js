bc7d9b771b4d7b20eccdb256143e0c21a0bc0f19
liuisaac <80558145+liuisaac@users.noreply.github.com>
2024-11-25 02:27:01 -0800

    hopefully fixed background for stars

--- src/pages/public/Companion/components/dataverse/Progress.js
+++ src/pages/public/Companion/components/dataverse/Progress.js
@@ -245,42 +245,6 @@ const Divider = () => {
               fill="white"
             />
           </g>
-          <defs>
-            <filter
-              id="filter0_d_563_161"
-              x="0"
-              y="0"
-              width="79"
-              height="79"
-              filterUnits="userSpaceOnUse"
-            >
-              <feFlood result="BackgroundImageFix" />
-              <feColorMatrix
-                in="SourceAlpha"
-                type="matrix"
-                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                result="hardAlpha"
-              />
-              <feOffset dy="2" />
-              <feGaussianBlur stdDeviation="10" />
-              <feComposite in2="hardAlpha" operator="out" />
-              <feColorMatrix
-                type="matrix"
-                values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.5 0"
-              />
-              <feBlend
-                mode="normal"
-                in2="BackgroundImageFix"
-                result="effect1_dropShadow_563_161"
-              />
-              <feBlend
-                mode="normal"
-                in="SourceGraphic"
-                in2="effect1_dropShadow_563_161"
-                result="shape"
-              />
-            </filter>
-          </defs>
         </svg>
       </Box>
 
@@ -307,42 +271,6 @@ const Divider = () => {
               fill="white"
             />
           </g>
-          <defs>
-            <filter
-              id="filter0_d_563_161"
-              x="0"
-              y="0"
-              width="79"
-              height="79"
-              filterUnits="userSpaceOnUse"
-            >
-              <feFlood result="BackgroundImageFix" />
-              <feColorMatrix
-                in="SourceAlpha"
-                type="matrix"
-                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                result="hardAlpha"
-              />
-              <feOffset dy="2" />
-              <feGaussianBlur stdDeviation="10" />
-              <feComposite in2="hardAlpha" operator="out" />
-              <feColorMatrix
-                type="matrix"
-                values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.5 0"
-              />
-              <feBlend
-                mode="normal"
-                in2="BackgroundImageFix"
-                result="effect1_dropShadow_563_161"
-              />
-              <feBlend
-                mode="normal"
-                in="SourceGraphic"
-                in2="effect1_dropShadow_563_161"
-                result="shape"
-              />
-            </filter>
-          </defs>
         </svg>
       </Box>
 
@@ -369,42 +297,6 @@ const Divider = () => {
               fill="white"
             />
           </g>
-          <defs>
-            <filter
-              id="filter0_d_563_161"
-              x="0"
-              y="0"
-              width="79"
-              height="79"
-              filterUnits="userSpaceOnUse"
-            >
-              <feFlood result="BackgroundImageFix" />
-              <feColorMatrix
-                in="SourceAlpha"
-                type="matrix"
-                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                result="hardAlpha"
-              />
-              <feOffset dy="2" />
-              <feGaussianBlur stdDeviation="10" />
-              <feComposite in2="hardAlpha" operator="out" />
-              <feColorMatrix
-                type="matrix"
-                values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.5 0"
-              />
-              <feBlend
-                mode="normal"
-                in2="BackgroundImageFix"
-                result="effect1_dropShadow_563_161"
-              />
-              <feBlend
-                mode="normal"
-                in="SourceGraphic"
-                in2="effect1_dropShadow_563_161"
-                result="shape"
-              />
-            </filter>
-          </defs>
         </svg>
       </Box>
     </Box>
